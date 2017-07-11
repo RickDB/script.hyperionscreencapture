8c6703fe551d12153e15c5bc3259d8c59afb00c4
RickDB <RickDB@gmail.com>
2017-07-11 02:21:08 +0200

    Made toggle a separate function.

--- service.py
+++ service.py
@@ -2,6 +2,21 @@
 import xbmc
 import urllib2
 
+def toggle_hyperioncapture(turnOn):
+    api_base_url = 'http://localhost:29445/API'
+
+    if turnOn:
+        url = '%s?command=ON' % (api_base_url)
+    else:
+        url = '%s?command=OFF' % (api_base_url)
+
+    try:
+        response = urllib2.urlopen(url)
+        response.read()
+        pass
+    except:
+        pass
+
 class Monitor(xbmc.Monitor):
     def __init__(self):
         xbmc.Monitor.__init__(self)
@@ -9,35 +24,17 @@ class Monitor(xbmc.Monitor):
     def onNotification(self, sender, method, data):
         global susppend_auto_change
         global set_for_susspend
-        api_base_url = 'http://localhost:29445/API'
 
         if 'Player.OnStop' in method:
-            try:
-                url = '%s?command=OFF' % (api_base_url)
-                response = urllib2.urlopen(url)
-                response.read()
-                pass
-            except:
-                pass
+            toggle_hyperioncapture(False)
         if 'Player.OnPlay' in method:
-            try:
-                url = '%s?command=ON' % (api_base_url)
-                response = urllib2.urlopen(url)
-                response.read()
-                pass
-            except:
-                pass
-
-# Disable on first start in case Kodi was closed improperly and left it on
-try:
-    url = '%s?command=OFF' % (api_base_url)
-    response = urllib2.urlopen(url)
-    response.read()
-    pass
-except:
-    pass
+            toggle_hyperioncapture(True)
 
 monitor = Monitor()
+
+# Disable on first start in case Kodi was closed improperly and left it on
+toggle_hyperioncapture(False)
+
 
 while (not xbmc.abortRequested):
     xbmc.sleep(100)
