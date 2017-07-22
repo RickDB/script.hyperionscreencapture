83a40c100bd2bd74249cbca7c5cdddfafb2c0818
RickDB <RickDB@gmail.com>
2017-07-22 13:35:28 +0200

    Added optional lock file support for remote automation. Added force parameter to API request URL.

--- service.py
+++ service.py
@@ -1,14 +1,17 @@
 # -*- coding: utf-8 -*-
-import xbmc
+import os
 import urllib2
+import xbmc
 
-def toggle_hyperioncapture(turnOn):
+lock_file = 'hyperion.lock'
+
+def toggle_hyperioncapture(turnOn, force = False):
     api_base_url = 'http://localhost:29445/API'
 
     if turnOn:
-        url = '%s?command=ON' % (api_base_url)
+        url = '%s?command=ON&force=%s' % (api_base_url, force)
     else:
-        url = '%s?command=OFF' % (api_base_url)
+        url = '%s?command=OFF&force=%s' % (api_base_url,force)
 
     try:
         response = urllib2.urlopen(url)
@@ -25,15 +28,21 @@ class Monitor(xbmc.Monitor):
         global susppend_auto_change
         global set_for_susspend
 
-        if 'Player.OnStop' in method:
-            toggle_hyperioncapture(False)
-        if 'Player.OnPlay' in method:
-            toggle_hyperioncapture(True)
+        # If no lock file exists handle command
+        if not os.path.exists(lock_file):
+          if 'Player.OnStop' in method:
+              xbmc.log('[hyperionscreencapture] player stopped, disabling hyperionscreencapture', xbmc.LOGDEBUG)
+              toggle_hyperioncapture(False)
+          if 'Player.OnPlay' in method:
+              xbmc.log('[hyperionscreencapture] player started, enabling hyperionscreencapture', xbmc.LOGDEBUG)
+              toggle_hyperioncapture(True)
+          if 'Player.OnPause' in method:
+            pass
 
 monitor = Monitor()
 
 # Disable on first start in case Kodi was closed improperly and left it on
-toggle_hyperioncapture(False)
+toggle_hyperioncapture(False, True)
 
 
 while (not xbmc.abortRequested):
